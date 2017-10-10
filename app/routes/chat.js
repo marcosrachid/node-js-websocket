86341a390b9eb383f6a0ac5817c6b04f34d95522
Marcos Rachid <marcosrachid@gmail.com>
2017-10-10 00:57:10 -0300

    incluindo validação de envio de apelido

--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,9 +1,9 @@
 module.exports = function(application) {
 	application.post('/chat', function(req, res) {
-		res.render('chat');
+		application.app.controllers.chat.iniciaChat(application, req, res);
 	});
 
 	application.get('/chat', function(req, res) {
-		res.render('chat');
+		application.app.controllers.chat.iniciaChat(application, req, res);
 	});
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,5 @@
 module.exports = function(application) {
 	application.get('/', function(req, res) {
-		res.render('index');
+		application.app.controllers.index.index(application, req, res);
 	});
 }
